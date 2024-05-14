438ac8335afc9b0b6345a7ad92ad9abf13c101c6
vitor100110 <231037048@aluno.unb.br>
2024-05-14 01:14:51 -0300

    :lipstick: feat:  Adiciona efeito hover para botões na tela de login

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -2,17 +2,18 @@ export default function Home() {
     return (
       <main className="text-black bg-gray-200 flex">
         <img src="/fundo.webp" alt="foto_de_fundo" className="w-1/2 h-screen" />
-        <section className="flex-col justify-center w-1/2 items-center flex p-32">
+        <section className="flex-col justify-center items-center flex p-32 w-1/2">
           <h1 className="mb-20 text-3xl font-semibold">
             Avaliação de professores
           </h1>
           <input className="p-2 m-3 rounded-lg w-full" type="text" placeholder="nome" />
           <input className="p-2 m-3 rounded-lg w-full" type="text" placeholder="senha" />
           <div className="flex flex-row">
-            <button className="p-2 m-3 bg-green-300 rounded-lg">Entrar</button>
-            <button className="p-2 m-3 bg-green-300 rounded-lg">Criar conta</button>
+            <button className="transition-transform duration-300 hover:scale-110 p-2 m-3 bg-green-300 rounded-lg">Entrar</button>
+            <button className="transition-transform duration-300 hover:scale-110 p-2 m-3 bg-green-300 rounded-lg">Criar conta</button>
           </div>
         </section>
       </main>
     );
   }
+  
