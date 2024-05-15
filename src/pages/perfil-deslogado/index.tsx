5d496d97e4e755d460d6cc553858e43f66c62b58
Billian <willianppl1958@gmail.com>
2024-05-15 03:01:21 -0300

    :lipstick:feat: novo perfil

--- src/pages/perfil-deslogado/index.tsx
+++ src/pages/perfil-deslogado/index.tsx
@@ -1,28 +1,19 @@
-import React from "react";
+export default function PerfilDeslogado() {
+    return (
+        <div className="bg-gray-300 flex flex-col min-h-screen">
+            <div className="bg-green-200 h-16 w-full">
+              <img src="unb.png" alt="Logo UNB" className="flex h-10 mt-2 ml-2 " />
+                <section className="relative w-96 h-72 left-1/2 transform -translate-x-1/2 flexmt-6"> 
+                    <div className="inset-x-0 top-0 bg-green-400 h-40 rounded-xl mt-6 z-0"></div>
+                    <img src="itadori.jpg" alt="Foto do perfil" className="absolute flex border-2 border-green-500 rounded-full overflow-hidden object-cover h-auto w-24 -mt-12 z-10"/>
+                    <div className="inset-x-0 top-0 bg-white h-1/2 rounded-xl z-0">
+                    <h2 className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center text-xl font-bold text-gray-800">Billian</h2>
+                    <p className="text-center text-black font-arial">Brasília/DF</p>
+                    <p className="text-center text-black font-arial"> Curso de Lic. em Computação</p>
+                    </div>
 
-export default function Home() {
-  return (
-    <main className="bg-white-400 text-black flex h-screen">
-      <div className=" w-full p-8 bg-white rounded-lg shadow-md">
-        <h2 className="text-2xl font-semibold mb-4">Perfil de Usuário</h2>
-        <div className="mb-4">
-          <label htmlFor="nome" className="block text-sm font-medium text-gray-700">Nome:</label>
-          <p className="text-gray-800" id="nome">Billian</p>
+                </section>
+            </div>
         </div>
-        <div className="mb-4">
-          <label htmlFor="email" className="block text-sm font-medium text-gray-700">E-mail:</label>
-          <p className="text-gray-800" id="email">teste@gmail.com</p>
-        </div>
-        <div className="mb-4">
-          <label htmlFor="telefone" className="block text-sm font-medium text-gray-700">Telefone:</label>
-          <p className="text-gray-800" id="telefone">(61) 99999-9999</p>
-        </div>
-        <div className="mb-4">
-          <label htmlFor="endereco" className="block text-sm font-medium text-gray-700">Endereço:</label>
-          <p className="text-gray-800" id="endereco">UnB- Darcy Ribeiro</p>
-        </div>
-        <button className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600 transition-colors duration-300">Editar Perfil</button>
-      </div>
-    </main>
-  );
+    );
 }
