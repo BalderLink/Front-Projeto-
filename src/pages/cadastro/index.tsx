d4a4abc90004284364e1915bffd3d5894fcc8d18
vitor100110 <231037048@aluno.unb.br>
2024-05-27 19:12:07 -0300

    :bricks: ci: deixei em conformidade com a tela de login

--- src/pages/cadastro/index.tsx
+++ src/pages/cadastro/index.tsx
@@ -1,20 +1,20 @@
 export default function CadastroUsuario() {
     return (
-        <main className="text-black bg-gray-200 flex">
-            <img src="/fundo.webp" alt="background" className="w-1/2 h-screen" />
-            <section className="flex-col justify-center items-center flex p-32 w-1/2">
-                <h1 className="mb-20 text-3xl font-semibold">
+        <main className="text-black bg-gray-200 flex min-h-screen">
+            <img src="/fundo.webp" alt="background" className="w-1/2 h-auto object-cover" />
+            <section className="flex-col justify-center items-center flex p-8 w-1/2">
+                <h1 className="mb-8 text-3xl font-semibold text-center">
                     Cadastro Usuário
                 </h1>
                 <form className="w-full flex flex-col items-center">
-                <input className="p-2 m-3 rounded-lg w-full" type="text" placeholder="Nome" />
-                <input className="p-2 m-3 rounded-lg w-full" type="email" placeholder="Email" />
-                <input className="p-2 m-3 rounded-lg w-full" type="password" placeholder="Senha" />
-                <input className="p-2 m-3 rounded-lg w-full" type="text" placeholder="Curso" />
-                <input className="p-2 m-3 rounded-lg w-full" type="text" placeholder="Departamento" />
-                <div className="flex flex-row">
-                    <button className="transition-transform duration-300 hover:scale-110 p-2 m-3 bg-green-300 rounded-lg w-full" type="submit">Criar Conta</button>
-                </div>
+                    <input className="p-2 mb-4 rounded-lg w-3/4" type="text" placeholder="Nome" />
+                    <input className="p-2 mb-4 rounded-lg w-3/4" type="email" placeholder="Email" />
+                    <input className="p-2 mb-4 rounded-lg w-3/4" type="password" placeholder="Senha" />
+                    <input className="p-2 mb-4 rounded-lg w-3/4" type="text" placeholder="Curso" />
+                    <input className="p-2 mb-4 rounded-lg w-3/4" type="text" placeholder="Departamento" />
+                    <div className="flex flex-row justify-center">
+                        <button className="transition-transform duration-300 hover:scale-110 p-2 mx-3 bg-green-300 rounded-lg w-full" type="submit">Criar Conta</button>
+                    </div>
                 </form>
             </section>
         </main>
