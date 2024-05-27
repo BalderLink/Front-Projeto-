4340ac30ef5bb53490b8aa8c34b85eef423f2774
vitor100110 <231037048@aluno.unb.br>
2024-05-27 18:58:25 -0300

    :bug: fix: tirei o flex do inicio pq tava faldno q tinha problema

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -2,19 +2,20 @@ export default function Home() {
     return (
         <main className="text-black bg-gray-200 flex min-h-screen">
             <img src="/fundo.webp" alt="foto_de_fundo" className="w-1/2 h-auto object-cover" />
-            <section className="flex flex-col justify-center items-center flex p-8 w-1/2">
+            <section className="flex-col justify-center items-center flex p-8 w-1/2">
                 <h1 className="mb-8 text-3xl font-semibold text-center">
                     Avaliação de professores
                 </h1>
                 <form className="w-full flex flex-col items-center">
-                    <input className="p-2 m-3 rounded-lg w-full" type="text" placeholder="nome" />
-                    <input className="p-2 m-3 rounded-lg w-full" type="password" placeholder="senha" />
-                    <div className="flex flex-row">
-                        <button className="transition-transform duration-300 hover:scale-110 p-2 m-3 bg-green-300 rounded-lg" type="submit">Entrar</button>
-                        <button className="transition-transform duration-300 hover:scale-110 p-2 m-3 bg-green-300 rounded-lg">Criar conta</button>
+                    <input className="p-2 mb-4 rounded-lg w-3/4" type="text" placeholder="Nome" />
+                    <input className="p-2 mb-4 rounded-lg w-3/4" type="password" placeholder="Senha" />
+                    <div className="flex flex-row justify-center">
+                        <button className="transition-transform duration-300 hover:scale-110 p-2 mx-3 bg-green-300 rounded-lg" type="submit">Entrar</button>
+                        <button className="transition-transform duration-300 hover:scale-110 p-2 mx-3 bg-green-300 rounded-lg">Criar conta</button>
                     </div>
                 </form>
             </section>
         </main>
     );
 }
+
