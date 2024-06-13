820bbffab3b417254bb1060dcbbb4577b6e98246
vitor100110 <231037048@aluno.unb.br>
2024-06-13 13:26:30 -0300

    nao tava rodando, desntinstalei e instalei de novo alguns arquivos, agora foi

--- src/components/modal.tsx
+++ src/components/modal.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 
 const Modal = ({ isOpen, onClose, children }) => {
   if (!isOpen) return null;
@@ -6,7 +6,10 @@ const Modal = ({ isOpen, onClose, children }) => {
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
       <div className="bg-white p-6 rounded shadow-md w-full max-w-md h-auto relative mx-auto">
-        <button onClick={onClose} className="absolute top-0 right-0 mt-4 mr-4 text-lg">
+        <button
+          onClick={onClose}
+          className="absolute top-0 right-0 mt-4 mr-4 text-lg"
+        >
           &times;
         </button>
         {children}
